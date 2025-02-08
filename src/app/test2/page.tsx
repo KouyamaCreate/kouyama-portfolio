18fec66a408b1b2cac4bdd7c190e5842c9a230f0
KouyamaCreate <kouyama.create@gmail.com>
2025-02-08 20:37:30 +0900

    Update page.tsx

--- src/app/test2/page.tsx
+++ src/app/test2/page.tsx
@@ -72,14 +72,14 @@ export default function Page() {
                             mesh.material.forEach((material) => {
                                 if (material.name === targetMaterialName) {
                                     //(material as THREE.MeshStandardMaterial).map = new THREE.TextureLoader().load("/textures/test.png");
-                                    let video = document.createElement('video');
+                                    const video = document.createElement('video');
                                     video.src = "/textures/test.mp4";
                                     video.muted = true;
                                     video.loop = true;
                                     video.load();
                                     video.play();
                                     // 動画テクスチャ作成
-                                    let texture = new THREE.VideoTexture(video);
+                                    const texture = new THREE.VideoTexture(video);
                                     // 1テクセルが1ピクセルより大きな範囲をカバーするときのテクスチャサンプリング方法の指定
                                     texture.magFilter = THREE.LinearFilter;
                                     // 1テクセルが1ピクセルより小さな範囲をカバーするときのテクスチャサンプリング方法の指定
@@ -93,13 +93,13 @@ export default function Page() {
                             // 単一マテリアルの場合
                             if (mesh.material.name === targetMaterialName) {
                                 //(mesh.material as THREE.MeshStandardMaterial).map = new THREE.TextureLoader().load("/textures/test.png");
-                                let video = document.createElement('video');
+                                const video = document.createElement('video');
                                 video.src = "/textures/test.mp4";
                                 video.muted = true;
                                 video.loop = true;
                                 video.load();
                                 // 動画テクスチャ作成
-                                let texture = new THREE.VideoTexture(video);
+                                const texture = new THREE.VideoTexture(video);
                                 // 1テクセルが1ピクセルより大きな範囲をカバーするときのテクスチャサンプリング方法の指定
                                 texture.magFilter = THREE.LinearFilter;
                                 // 1テクセルが1ピクセルより小さな範囲をカバーするときのテクスチャサンプリング方法の指定
